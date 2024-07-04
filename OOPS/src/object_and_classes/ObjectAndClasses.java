89f98ed6aa91352a52a111a592876bb5332c82da
Santosh Dahal <sirishsds@gmail.com>
2024-07-04 14:19:14 +0545

    Obj and Classes;

--- OOPS/src/object_and_classes/ObjectAndClasses.java
+++ OOPS/src/object_and_classes/ObjectAndClasses.java
@@ -1,8 +1,25 @@
 package object_and_classes;
 
+
+// Creating a  custom class
+class Employee{
+	int id;
+	String name;
+}
+
 public class ObjectAndClasses {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("This is my custom class..");
+		Employee emp = new Employee();// Crating a object
+		
+		//	Setting Attributes
+		emp.id=1;
+		emp.name="Santosh";
+
+		System.out.println(emp.id);
+		System.out.println(emp.name);
+
+
 	}
 
 }

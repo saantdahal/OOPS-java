d6d5551986362a61a085dc281c170d0f79d4d161
Santosh Dahal <sirishsds@gmail.com>
2024-07-06 15:20:40 +0545

    Chapter 8 practice set added

---
+++ OOPS/src/object_and_classes_chapter8_pr/Qno1.java
@@ -0,0 +1,29 @@
+// Program to create a Employee class with there respective properties
+package object_and_classes_chapter8_pr;
+
+// Creating a class Employee
+class Employee{
+	int salary;
+	String name;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
+
+public class Qno1 {
+	public static void main(String[] args) {
+
+	}
+
+}
